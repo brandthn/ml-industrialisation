566fe3d513fde6635c31ab176f6dad302b82994e
Brandon Tchaka NIYUNGEKO <127215245+brandthn@users.noreply.github.com>
2026-03-04 17:04:43 +0100

    [DONE] TD2 - 1

--- src/model.py
+++ src/model.py
@@ -1,9 +1,27 @@
 import pandas as pd
 
+
+class PrevMonthSale:
+    def predict(self, df):
+        return df.groupby("item_id")["sales"].shift(1)
+
+
+class SameMonthLastYearSales:
+    def predict(self, df):
+        return df.groupby("item_id")["sales"].shift(12)
+
+
+MODEL_REGISTRY = {
+    "PrevMonthSale": PrevMonthSale,
+    "SameMonthLastYearSales": SameMonthLastYearSales,
+}
+
+
 def make_predictions(config):
     df_sales = pd.read_csv(config["data"]["sales"])
 
-    df_sales["prediction"] = df_sales.groupby("item_id")["sales"].shift(1)
+    model = MODEL_REGISTRY[config["model"]]()
+    df_sales["prediction"] = model.predict(df_sales)
 
     df_sales = df_sales[df_sales["dates"] >= config["start_test"]].reset_index(drop=True)
 
